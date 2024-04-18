0fd74e893921515df52b9b9af9841a472617c49b
michaelpgibby <131623321+michaelpgibby@users.noreply.github.com>
2024-04-18 17:04:28 -0400

    Update planeconfig.py

--- planeconfig.py
+++ planeconfig.py
@@ -14,16 +14,15 @@ def index():
 @app.route('/update_config', methods=['POST'])
 def update_config():
     if request.method == 'POST':
+        # Convert brightness, min_altitude, and gpio_slowdown to integers
         new_config = request.form.to_dict()
-        # Convert specific fields to integers if they exist
-        for key in ['brightness', 'min_altitude', 'gpio_slowdown']:
-            if key in new_config:
-                try:
-                    new_config[key] = int(new_config[key])
-                except ValueError:
-                    return f"Error: {key} must be an integer."
+        new_config['brightness'] = int(new_config.get('brightness', config_data['brightness']))
+        new_config['min_altitude'] = int(new_config.get('min_altitude', config_data['min_altitude']))
+        new_config['gpio_slowdown'] = int(new_config.get('gpio_slowdown', config_data['gpio_slowdown']))
+
         # Update the config_data dictionary with the new values
         config_data.update(new_config)
+
         # Save the updated config back to web_config.json
         with open('web_config.json', 'w') as f:
             json.dump(config_data, f, indent=4)
