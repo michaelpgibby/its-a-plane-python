02c19cedff4aa8ec54ab1e594bc23c8922345447
michaelpgibby <131623321+michaelpgibby@users.noreply.github.com>
2024-04-18 14:35:08 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,26 @@ from setup import colours
 with open('web_config.json', 'r') as config_file:
     config_data = json.load(config_file)
 
+COLORS_MAP = {
+    "BLACK": colours.BLACK,
+    "WHITE": colours.WHITE,
+    "GREY": colours.GREY,
+    "YELLOW": colours.YELLOW,
+    "YELLOW_DARK": colours.YELLOW_DARK,
+    "BLUE": colours.BLUE,
+    "BLUE_LIGHT": colours.BLUE_LIGHT,
+    "BLUE_DARK": colours.BLUE_DARK,
+    "BLUE_DARKER": colours.BLUE_DARKER,
+    "PINK": colours.PINK,
+    "PINK_DARK": colours.PINK_DARK,
+    "PINK_DARKER": colours.PINK_DARKER,
+    "GREEN": colours.GREEN,
+    "ORANGE": colours.ORANGE,
+    "ORANGE_DARK": colours.ORANGE_DARK,
+    "RED": colours.RED,
+    "RED_LIGHT": colours.RED_LIGHT
+}
+
 ZONE_HOME = config_data['ZONE_HOME']
 LOCATION_HOME = config_data['LOCATION_HOME']
 WEATHER_LOCATION = config_data['WEATHER_LOCATION']
@@ -18,6 +38,6 @@ GPIO_SLOWDOWN = config_data['GPIO_SLOWDOWN']
 JOURNEY_CODE_SELECTED = config_data['JOURNEY_CODE_SELECTED']
 JOURNEY_BLANK_FILLER = config_data['JOURNEY_BLANK_FILLER']
 HAT_PWM_ENABLED = config_data['HAT_PWM_ENABLED']
-AIRPORT_COLOR = colours.get(config_data['AIRPORT_COLOR'], colours.BLACK)
-ARROW_COLOR = colours.get(config_data['ARROW_COLOR'], colours.BLACK)
-PLANE_DETAILS_COLOR = colours.get(config_data['PLANE_DETAILS_COLOR'], colours.BLACK)
+AIRPORT_COLOR = COLORS_MAP.get(config_data['AIRPORT_COLOR'], colours.WHITE)
+ARROW_COLOR = COLORS_MAP.get(config_data['ARROW_COLOR'], colours.WHITE)
+PLANE_DETAILS_COLOR = COLORS_MAP.get(config_data['PLANE_DETAILS_COLOR'], colours.WHITE)
