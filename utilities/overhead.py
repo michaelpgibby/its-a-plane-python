c084b6cf7ee8a4e23445086b57edfd0c141fac91
michaelpgibby <131623321+michaelpgibby@users.noreply.github.com>
2024-04-18 22:56:49 -0400

    Update overhead.py
    moving flight class

--- utilities/overhead.py
+++ utilities/overhead.py
@@ -48,12 +48,7 @@ def distance_from_flight_to_home(flight, home=LOCATION_DEFAULT):
 
     (x, y, z) = polar_to_cartesian(flight.latitude, flight.longitude, flight.altitude)
     return math.sqrt(x**2 + y**2 + z**2)
-    
-class Flight:
-    def __init__(self, latitude, longitude, altitude):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.altitude = altitude
+
 
     def feet_to_meters_plus_earth(altitude_ft):
         altitude_km = 0.0003048 * altitude_ft
@@ -75,7 +70,12 @@ class Flight:
     except AttributeError:
         # on error say it's far away
         return 1e6
-
+        
+class Flight:
+    def __init__(self, latitude, longitude, altitude):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
 
 class Overhead:
     def __init__(self):
