0d34bd2bd0b30bf521972a0c59290a28cc48599a
michaelpgibby <131623321+michaelpgibby@users.noreply.github.com>
2024-04-19 14:15:02 -0400

    Update overhead.py

--- utilities/overhead.py
+++ utilities/overhead.py
@@ -43,12 +43,9 @@ def distance_from_flight_to_home(flight, home=LOCATION_DEFAULT):
             alt * math.sin(DEG2RAD * lat),
             alt * math.cos(DEG2RAD * lat) * math.cos(DEG2RAD * long),
         ]
-   # if not isinstance(flight.altitude, (int, float)):
-    #        raise ValueError("Altitude must be a single numerical value")
-
-    #(x, y, z) = polar_to_cartesian(flight.latitude, flight.longitude, flight.altitude)
-    #return math.sqrt(x**2 + y**2 + z**2)
-
+   
+    # Debug print to show the values of latitude, longitude, and altitude
+    print(f"Latitude: {flight.latitude}, Longitude: {flight.longitude}, Altitude: {flight.altitude}")
 
     def feet_to_meters_plus_earth(altitude_ft):
         altitude_km = 0.0003048 * altitude_ft
@@ -70,12 +67,7 @@ def distance_from_flight_to_home(flight, home=LOCATION_DEFAULT):
     except AttributeError:
         # on error say it's far away
         return 1e6
-        
-#class Flight:
- #   def __init__(self, latitude, longitude, altitude):
-  #      self.latitude = latitude
-  #      self.longitude = longitude
-   #     self.altitude = altitude
+
 
 class Overhead:
     def __init__(self):
